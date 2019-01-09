bce4c10862934f03606067546ba6f9bb0f3c0856
Samik Pradhan <samikpradhan@gmail.com>
2019-01-09 17:43:32 +0000

    started SQL stuff

--- CODE/html/titleSearch.php
+++ CODE/html/titleSearch.php
@@ -13,14 +13,14 @@
       $host = "127.0.0.1";
       $user = "appoxalypse";
       $pass = "";
-      $db = "web_novels"; //change this part later
+      $db = "web_novel";
       $port = 3306;
       $connection = mysqli_connect($host, $user, $pass, $db, $port) or die(mysql_error());
 
       //Call data from the database
       if (isset($_POST['submit'])) {
 
-        echo "Web Novels with titles that start with the letter: " . $_POST['alphabet']; //maybe divide this into 2 lines
+        echo "Web Novels with titles that start with the letter: " . $_POST['alphabet'];
 
 
         // key ID, title, author, genre, rating, alphabet
@@ -39,7 +39,7 @@
             echo '<tr><td align="left">' . $row[title] .
                   '</td><td align="left">' . $row[author] .
                   '</td><td align="left">' . $row[genre] .
-                  '</td><td align="left">' . $row[rating]
+                  '</td><td align="left">' . $row[rating] .
                   '</td></tr>';
           }
 
